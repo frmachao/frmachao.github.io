59bcf119a717314c40be3644cdd14d8cf25a0866
frmachao <frmachao@126.com>
2021-03-21 15:32:29 +0800

    Site updated: 2021-03-21 15:32:29

--- js/leancloud.js
+++ js/leancloud.js
@@ -73,8 +73,8 @@
     var key = 'LeanCloud_UV_Flag';
     var flag = localStorage.getItem(key);
     if (flag) {
-      // 距离标记小于 24 小时则不计为 UV
-      if (new Date().getTime() - parseInt(flag, 10) <= 86400000) {
+      // 距离标记小于 6 小时则不计为 UV
+      if (new Date().getTime() - parseInt(flag, 10) <= 28800000) {
         return false;
       }
     }
